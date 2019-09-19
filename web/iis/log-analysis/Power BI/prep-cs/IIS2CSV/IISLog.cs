3f04a2df69c2d3123b152d3eafa54f153d458748
Joy George Kunjikkuru <joy.g@Orioninc.com>
2019-09-19 16:22:35 -0400

    Added CS based  IIS2CSV converted

---
+++ web/iis/log-analysis/Power BI/prep-cs/IIS2CSV/IISLog.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+
+namespace IIS2CSV
+{
+    class IISLog
+    {
+        [DataMember(Name = "date")]
+        public string date { get; set; }
+
+        [DataMember(Name = "time")]
+        public string time { get; set; }
+
+        [DataMember(Name = "s-ip")]
+        public string sIP { get; set; }
+        [DataMember(Name = "cs-method")]
+        public string csMethod { get; set; }
+        [DataMember(Name = "cs-uri-stem")]
+        public string csuristem { get; set; }
+        [DataMember(Name = "cs-uri-query")]
+        public string csuriquery { get; set; }
+        [DataMember(Name = "s-port")]
+        public int? sport { get; set; }
+        [DataMember(Name = "cs-username")]
+        public string csusername { get; set; }
+        [DataMember(Name = "c-ip")]
+        public string cIP { get; set; }
+        [DataMember(Name = "cs-user-agent")]
+        public string csuseragent { get; set; }
+        [DataMember(Name = "sc-status")]
+        public int? scStatus { get; set; }
+        [DataMember(Name = "sc-substatus")]
+        public int? scsubstatus { get; set; }
+        [DataMember(Name = "sc-win32-status")]
+        public long? scwin32status { get; set; }
+        [DataMember(Name = "sc-bytes")]
+        public int? scbytes { get; set; }
+        [DataMember(Name = "cs-bytes")]
+        public int? csbytes { get; set; }
+        [DataMember(Name = "time-taken")]
+        public int? timetaken { get; set; }
+    }
+}
---
+++ web/iis/log-analysis/Power BI/prep-cs/IIS2CSV/Program.cs
@@ -0,0 +1,68 @@
+﻿using IISLogParser;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IIS2CSV
+{
+    partial class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            CsvSerializer serializer = new CsvSerializer();
+
+            //string FilePath = @"C:\temp\KCw\Sep 2019\IIS Analysis\us-wclclaweb03\W3SVC8989\u_ex190901.log";
+            //IIS2CSV(FilePath);
+            string folderPath = @"C:\temp\KCw\Sep 2019\IIS Analysis\us-wclclaweb03\W3SVC8989\";
+            foreach (string filePath in Directory.GetFiles(folderPath, "*.log"))
+            {
+                IIS2CSV(filePath);
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void IIS2CSV(string FilePath)
+        {
+
+            string outPath = Path.ChangeExtension(FilePath, "csv");
+            Console.WriteLine($"Starting {outPath}");
+            using (ParserEngine parser = new ParserEngine(FilePath))
+            {
+                IList<IISLog> logs = null;
+                while (parser.MissingRecords)
+                {
+                    logs = parser.ParseLog().Select(rec => new IISLog()
+                    {
+                        date = rec.DateTimeEvent.ToString("yyyy-MM-dd"),
+                        time = rec.DateTimeEvent.ToString("HH:mm:ss"),
+                        sIP = rec.sIp,
+                        csMethod = rec.csMethod,
+                        csuristem = rec.csUriStem,
+                        csuriquery = rec.csUriQuery,
+                        sport = rec.sPort,
+                        csusername = rec.csUsername,
+                        cIP = rec.cIp,
+                        csuseragent = rec.csUserAgent,
+                        scStatus = rec.scStatus,
+                        scsubstatus = rec.scSubstatus,
+                        scwin32status = rec.scWin32Status,
+                        scbytes = rec.scBytes,
+                        csbytes = rec.csBytes,
+                        timetaken = rec.timeTaken
+                    }).ToList();
+                    Console.WriteLine($"Length {logs.Count}");
+                }
+                Console.WriteLine($"Writing to {outPath}");
+                using (TextWriter writer = File.CreateText(outPath))
+                {
+                    CsvSerializer.SerializeToWriter<IList<IISLog>>(logs, writer);
+                }
+            }
+        }
+    }
+}
